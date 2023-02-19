95fcaa8ccf6662b83d2c47a43ff72af190c5ef61
akashnanda <45444456+akashnanda@users.noreply.github.com>
2023-02-19 19:30:59 +0530

    Add files via upload

---
+++ New1/src/App.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MainContainer from "./components/MainContainer";
+import LeftMenu from "./components/LeftMenu";
+// import TopMenu from "./components/TopMenu";
+// import ListContainer from "./components/ListContainer";
+import UserListComponent from './components/UserListComponent';
+import TodoListComponent from './components/TodoListComponent';
+
+import {BrowserRouter as Router,Route,Routes} from "react-router-dom";
+import { Button } from '@mui/material';
+function App() {
+  
+  return (
+<>
+       <Routes>
+          <Route path="/" element={<UserListComponent />} />
+          <Route path="/user" element={<UserListComponent />} />
+          <Route path="/todo" element={<TodoListComponent />} />
+       </Routes>
+
+       <div>
+        <MainContainer>
+          <LeftMenu />
+        </MainContainer>
+      </div>
+
+
+    </>
+    // <>
+    //   <Routes>
+       
+    //     <Route  path="/user" element={<UserListComponent></UserListComponent>}></Route>
+    //     <Route  path="/todo" element={<TodoListComponent></TodoListComponent>}></Route>
+        
+    //   </Routes>
+   
+   
+    //   </>
+  );
+}
+
+export default App;
